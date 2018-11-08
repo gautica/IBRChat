b1aca2a2b859d7b370229f5dd54c632ec794833f
yan <Yan@NerdGautica.com>
2018-11-08 20:32:46 +0100

    usage

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,8 +4,19 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        printf("Usage: <server> <IP> [port_to_client] [port_to_server] [port_from_server]\n");
+        exit(EXIT_FAILURE);
+    }
+    /**
+     * argv[1]: IP (required)
+     * argv[2]: port to client (optional)
+     * argv[3]: port to server (optional)
+     * argv[4]: port from server (optional)
+     */
     Server server(argv[1], argv[2], argv[3], argv[4]);
 
     server.start();
+
     return 0;
 }
